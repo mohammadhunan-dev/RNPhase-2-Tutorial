6cba0434b0259a439f4c3d1c0b3d05dce3dbd6a9
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-08-25 15:15:28 -0400

    added project view and task view

--- providers/AuthProvider.js
+++ providers/AuthProvider.js
@@ -1,6 +1,6 @@
 import React, {useContext, useState} from 'react';
 import Realm from 'realm';
-import {getRealmApp} from './getRealmApp';
+import {getRealmApp} from '../getRealmApp';
 
 // Access the Realm App.
 const app = getRealmApp();
@@ -9,9 +9,43 @@ const app = getRealmApp();
 const AuthContext = React.createContext(null);
 
 const AuthProvider = ({children}) => {
-  const login = () => null;
-  const logout = () => null;
-  const registerUser = () => null;
+  const signIn = async (email, password) => {
+    try {
+      const credentials = Realm.Credentials.emailPassword(email, password);
+      const user = await app.logIn(credentials);
+      const userRealmConfig = {
+        sync: {
+          user: user,
+          partitionValue: `user=${user.id}`,
+        },
+      };
+      const userRealm = await Realm.open(userRealmConfig);
+      return userRealm;
+    } catch (err) {
+      throw 'Error signing in' + err;
+    }
+  };
+  const signUp = async (email, password) => {
+    try {
+      await app.emailPasswordAuth.registerUser(email, password);
+      return await signIn(email, password);
+    } catch (err) {
+      throw 'Error signing up ' + JSON.stringify(err, null, 2);
+    }
+  };
+  const logOut = () => null;
+  let user = null;
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn,
+        signUp,
+        logOut,
+        user,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 const useAuth = () => {
